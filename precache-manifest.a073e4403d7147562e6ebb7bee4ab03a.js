b1cff53d8af8c9333273da3fa3146890250b97d9
unknown <karnaa787@gmail.com>
2020-04-29 13:53:37 +0545

    Updates

---
+++ precache-manifest.a073e4403d7147562e6ebb7bee4ab03a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e4d7e5decb44b69288f8bc4005318a",
+    "url": "/robofriendsreact/index.html"
+  },
+  {
+    "revision": "64bbf207e3008ff07368",
+    "url": "/robofriendsreact/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "4d29037dd2ad17c25f26",
+    "url": "/robofriendsreact/static/css/main.1fb34233.chunk.css"
+  },
+  {
+    "revision": "64bbf207e3008ff07368",
+    "url": "/robofriendsreact/static/js/2.4ccd42c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriendsreact/static/js/2.4ccd42c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d29037dd2ad17c25f26",
+    "url": "/robofriendsreact/static/js/main.b11c7842.chunk.js"
+  },
+  {
+    "revision": "cb9db1e3b770ab8b9f37",
+    "url": "/robofriendsreact/static/js/runtime-main.a763c20e.js"
+  }
+]);
